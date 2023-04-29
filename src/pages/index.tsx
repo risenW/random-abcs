23aeed58f986db3a6ced438f951e7af053a116f5
Rising Odegua <risingodegua@gmail.com>
2023-04-29 18:20:01 +0100

    add images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,22 @@
 import Link from 'next/link';
 import BaseLayout from '../components/Layout';
-import { Card, message } from 'antd';
+import { Card } from 'antd';
 import Image from 'next/image';
 const { Meta } = Card;
 
 
 export default function Home() {
-  const [messageApi, contextHolder] = message.useMessage();
 
   return (
     <BaseLayout title='Random - ABCs - Generate random numbers, colors, names, countries, and more'>
-      {contextHolder}
-      <div className='flex flex-col'>
+      <div className=''>
         <h1 className='text-xl font-bold mb-5'>Random Generators</h1>
         <div className='grid gap-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
-          <Link href={"/random-group"}>
+          <Link href={"/random-group"} >
             <Card
               hoverable
               cover={<Image
-                src="/random-group.png"
+                src="/random-grp.svg"
                 width={200}
                 height={200}
                 alt='Random Group'
@@ -37,7 +35,7 @@ export default function Home() {
             <Card
               hoverable
               cover={<Image
-                src="/random-number.png"
+                src="/random-num.svg"
                 width={200}
                 height={200}
                 alt='Random Number'
@@ -55,7 +53,7 @@ export default function Home() {
             <Card
               hoverable
               cover={<Image
-                src="/throw-dice.png"
+                src="/dices.svg"
                 width={200}
                 height={200}
                 alt='Throw Dice'
@@ -73,7 +71,7 @@ export default function Home() {
             <Card
               hoverable
               cover={<Image
-                src="/random-color.png"
+                src="/random-color.svg"
                 width={200}
                 height={200}
                 alt='Random Color'
@@ -91,7 +89,7 @@ export default function Home() {
             <Card
               hoverable
               cover={<Image
-                src="/flip-coin.png"
+                src="/coin.svg"
                 width={200}
                 height={200}
                 alt='Flip Coin'
@@ -109,7 +107,7 @@ export default function Home() {
             <Card
               hoverable
               cover={<Image
-                src="/random-countries.png"
+                src="/country.svg"
                 width={200}
                 height={200}
                 alt='Random Countries'
@@ -133,7 +131,7 @@ export default function Home() {
               <Card
                 hoverable
                 cover={<Image
-                  src="/json-formatter.png"
+                  src="/json-file.svg"
                   width={200}
                   height={200}
                   alt='JSON Formatter'
@@ -147,11 +145,11 @@ export default function Home() {
                 />
               </Card>
             </Link>
-            <Link href={"/json-to-csv"}>
+            {/* <Link href={"/json-to-csv"}>
               <Card
                 hoverable
                 cover={<Image
-                  src="/json-to-csv.png"
+                  src="/jsontocsv.png"
                   width={200}
                   height={200}
                   alt='JSON to CSV'
@@ -164,12 +162,12 @@ export default function Home() {
                   description="Load, parse and convert JSON data to CSV format."
                 />
               </Card>
-            </Link>
+            </Link> */}
             <Link href={"/csv-to-json"}>
               <Card
                 hoverable
                 cover={<Image
-                  src="/csv-to-json.png"
+                  src="/csvtojson.png"
                   width={200}
                   height={200}
                   alt='CSV to JSON'
