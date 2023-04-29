6a7ad1dd4c141aa6c9e9329017a3132bc2b48746
Rising Odegua <risingodegua@gmail.com>
2023-04-29 14:56:11 +0100

    Add some SEO support

---
+++ next-seo.config.js
@@ -0,0 +1,14 @@
+const SEO = {
+    title: 'random-abcs | Home | Generate randoms, utilities for developers',
+    description: 'Generate random numbers, colors, names, countries, text. Utilities for developers to manage JSON, CSV, XML, YAML, HTML, CSS, JS, SQL, Markdown, and more.',
+    openGraph: {
+        type: 'website',
+        locale: 'en_IE',
+        url: 'https://www.randomabcs.com',
+        title: 'random-abcs | Home | Generate randoms, utilities for developers',
+    },
+    
+};
+
+
+export default SEO;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react';
+import SEO from '../../next-seo.config';
+
+import { DefaultSeo } from 'next-seo';
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <DefaultSeo {...SEO} />
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
